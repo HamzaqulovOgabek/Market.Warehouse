e7f9a514d35e92f45eaa472ed0ba85d4f3da5d81
Ogabek <hamzaqulovogabek@gmail.com>
2024-08-24 18:34:52 +0500

    Added Migration

---
+++ Market.Warehouse.DataAccess/Migrations/20240824131158_AddedWareHouseTableAndStateToTables.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Market.Warehouse.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedWareHouseTableAndStateToTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Review",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "ProductImage",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "CartItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Brand",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Warehouse",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Warehouse", x => x.Id);
+                });
+
+            
+
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_Warehouse_WareHouseId",
+                table: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Warehouse");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_WareHouseId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Review");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "ProductImage");
+
+            migrationBuilder.DropColumn(
+                name: "WareHouseId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "CartItem");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Brand");
+        }
+    }
+}
---
+++ Market.Warehouse.DataAccess/Migrations/20240824132429_AddedWareHouseIdForeignKeyToProduct.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Market.Warehouse.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedWareHouseIdForeignKeyToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WareHouseId",
+                table: "Product",
+                type: "int",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_WareHouseId",
+                table: "Product",
+                column: "WareHouseId");
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_Warehouse_WareHouseId",
+                table: "Product",
+                column: "WareHouseId",
+                principalTable: "Warehouse",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
