778ae320d2a16ee7119f5aae894fd2939998c822
Ogabek <hamzaqulovogabek@gmail.com>
2024-09-08 11:50:21 +0500

    Added  Products to Database

---
+++ Market.Warehouse/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Market.Warehouse.DataAccess.Context;
+using Market.Warehouse.DataAccess.SeedData;
+
+namespace Market.Warehouse.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    internal static IApplicationBuilder SeedDatabase(this IApplicationBuilder app)
+    {
+        using var serviceScope = app.ApplicationServices.CreateScope();
+        var seeders = serviceScope.ServiceProvider.GetServices<AppDbContext>();
+
+        string jsontext = File.ReadAllText("SeedData/Products.json");
+        Seeding.SeedAsync(jsontext, app.ApplicationServices).GetAwaiter().GetResult();
+
+        return app;
+
+    }
+
+}
---
+++ Market.Warehouse/SeedData/Seeding.cs
@@ -0,0 +1,25 @@
+﻿using Market.Warehouse.DataAccess.Context;
+using Market.Warehouse.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Market.Warehouse.DataAccess.SeedData
+{
+    public class Seeding
+    {
+        public static async Task SeedAsync(string jsonData, IServiceProvider serviceProvider)
+        {
+            var products = JsonConvert.DeserializeObject<List<Product>>(jsonData);
+            var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope();
+            var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+            if(! await context.Products.AnyAsync())
+            {
+                await context.AddRangeAsync(products);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
